bc6fe6a626d7083b94694d0b56a4a88e1b0e8ce9
Lee Echo <36832801+leekh0314@users.noreply.github.com>
2019-06-02 18:31:18 +0800

    commit turtle by Python：
    2019.06.02

---
+++ Python turtle.py
@@ -0,0 +1,21 @@
+import turtle
+
+my_turtule = turtle.Turtle()
+
+my_turtule.shape("turtle")
+
+my_turtule.forward(50)
+my_turtule.right(90)
+my_turtule.forward(50)
+my_turtule.right(90)
+my_turtule.forward(50)
+my_turtule.right(90)
+my_turtule.forward(50)
+my_turtule.right(90)
+
+turtle.mainloop( )
+
+"""
+Error错误提示:
+17:56	Error running 'Python turtle': Cannot run program "F:\Python Pycharm\Project all\test02\venv\Scripts\python.exe" (in directory "F:\Python Pycharm\Project all\Python-project"): CreateProcess error=2, 系统找不到指定的文件。
+"""
