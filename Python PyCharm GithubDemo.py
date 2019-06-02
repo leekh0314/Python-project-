315c324527a805d834bb5f5a48fcbf2fba77612f
Administrator <903828673@qq.com>
2019-06-02 17:43:56 +0800

    第二次上传GitHub first commit ：python pycharm IED to GitHub Demo

--- Python PyCharm GithubDemo.py
+++ Python PyCharm GithubDemo.py
@@ -1,3 +1,12 @@
 import turtle
 #
-"""GitHub上传与克隆代码练习1"""
+
+#pycharm 编辑器社区版本安装，PyCharm官方网站下载Community社区免费版。
+#python3.0版本安装
+#Git工具安装
+
+"""如何通过pycharm上传代码至GitHub？"""
+#第一步：安装Git工具
+#第二步：pycharm界面-顶部菜单栏-VCS-checkout from version control-Git，导入进pycharm
+#第三步：pycharm代码编辑界面-右击-Git-add；然后再次右击-Git-commit file，编辑相关更新说明信息后，push，上传GitHub
+"""GitHub上传与克隆代码练习2"""
